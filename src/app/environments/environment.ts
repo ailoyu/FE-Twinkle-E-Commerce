768e0e1719321d3767afb92befecaaed6b6f818f
CHAN QUANG <quangtrinhhuynh02@gmail.com>
2023-09-29 10:17:41 +0700

    deploy

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://twinkleee-0f5f3ae8991e.herokuapp.com/',
+    apiBaseUrl: 'https://twinkleee-0f5f3ae8991e.herokuapp.com/api/v1',
 };
