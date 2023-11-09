eacee0bfdaec5215c817a158f406fcf49701bf07
an <ngoquocan04112002@gmail.com>
2023-11-10 04:05:45 +0700

    deploy

--- src/app/components/admin/carousel/carousel.component.ts
+++ src/app/components/admin/carousel/carousel.component.ts
@@ -61,23 +61,23 @@ export class CarouselComponent implements OnInit{
   imagesnicexu = [
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      imageAlt: 'nature1',
+        './assets/images/vans.gif',
+      imageAlt: 'vans1',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      imageAlt: 'nature2',
+        './assets/images/nike.gif',
+      imageAlt: 'nike2',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1640844444545-66e19eb6f549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
-      imageAlt: 'person1',
+        './assets/images/adidas.gif',
+      imageAlt: 'adidas3',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      imageAlt: 'person2',
+        './assets/images/converse.gif',
+      imageAlt: 'converse4',
     },
   ]
 
